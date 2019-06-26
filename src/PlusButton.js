10da97ef3a05e8e1792c20c146c673610aaf0a3b
woaidouya123 <cfeigood@qq.com>
2019-06-26 23:57:46 +0800

    修改菜单样式问题

--- src/PlusButton.js
+++ src/PlusButton.js
@@ -1,27 +1,29 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 
 class PlusButton extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.addMMO = this.addMMO.bind(this);
     }
     render() {
         return (
-            <div className="plus-button" onClick={this.addMMO}>
-                <img src={require("../public/plus.png")}/>
-                <ul>
+            <div className="plus-div">
+                <div className="plus-button" onClick={this.addMMO}>
+                    <img src={require("../public/plus.png")} />
+                </div><ul>
                     <li>Circle</li>
                     <li>Rectangle</li>
                     <li>Star</li>
                     <li>DIY</li>
                 </ul>
             </div>
-            
-            
+
+
+
         );
     };
-    addMMO(){
-        
+    addMMO() {
+
     }
 }
 module.exports = PlusButton;
