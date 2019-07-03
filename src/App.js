ba933f4709bff5c719329659eeea78a9e9b0d9f0
woaidouya123 <cfeigood@qq.com>
2019-07-04 00:03:58 +0800

    完善普通组件

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
                 <BgCanvas />
                 <PlusButton />
                 <NormalMMo message="hello" />
+                <NormalMMo message="world" />
             </div>
         );
     }
--- src/mmo/normalMMO.js
+++ src/mmo/normalMMO.js
@@ -3,14 +3,51 @@ import React, { Component } from "react";
 class NormalMMo extends Component {
     constructor(props) {
         super(props);
+        this.mousedown = this.mousedown.bind(this);
+        this.mousemove = this.mousemove.bind(this);
+        this.mouseup = this.mouseup.bind(this);
+        this.state = {
+            "isDraging" : false,
+            "startX":0,
+            "startY":0
+        }
     }
     render() {
         return (
-            <div className="normal-div">
+            <div className="normal-div" onMouseDown={this.mousedown} onMouseMove={this.mousemove} onMouseUp={this.mouseup}>
                 <span>{this.props.message}</span>
-                <span className="finish-button"></span>
+                <label>
+                    <input type="checkbox" style={{display:"none"}} />
+                    <span className="finish-button"></span>
+                </label>
+                
             </div>
         );
     };
+
+    componentDidMount(){
+        
+    }
+
+    // 拖拽
+    mousedown(event) {
+        this.setState({isDraging:true});
+        console.log("mouse",event.clientX,event.clientY);
+        var positionX = parseInt(event.target.style.left),
+            positionY = parseInt(event.target.style.top);
+        positionX = positionX || event.target.scrollLeft;
+        positionY = positionY || event.target.scrollTop;
+        this.setState({paddingX:event.clientX - positionX});
+        this.setState({paddingY:event.clientY - positionY})
+    }
+    mousemove(event) {
+        if(this.state.isDraging){
+            event.target.style.left = (event.clientX - this.state.paddingX) + "px";
+            event.target.style.top = (event.clientY - this.state.paddingY) + "px";
+        }
+    }
+    mouseup(event) {
+        this.setState({isDraging:false});
+    }
 }
 module.exports = NormalMMo;
